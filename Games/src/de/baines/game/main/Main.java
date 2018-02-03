e1487d93f6f200e0917516292d4fafd23f382c3b
baine <baine@192.168.0.6>
2018-02-04 00:06:55 +0100

    test commit from laptop

--- Games/src/de/baines/game/main/Main.java
+++ Games/src/de/baines/game/main/Main.java
@@ -11,6 +11,7 @@ public class Main {
 		
 	    int DISPLAYHEIGHT = 600;
 	    int DISPLAYWIDTH = 800;
+	    System.out.println("test");
 		
 		Player player = new Player(300,300,100,DISPLAYHEIGHT,DISPLAYWIDTH);
 		KeyHandler keyboard = new KeyHandler();
